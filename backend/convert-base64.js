4233642d5c8f47efeb7ca1f9637c03a50163fb9d
Bagas Arya Wijaya <bagasaryawijaya27@gmail.com>
2026-09-04 19:58:50 +0700

    Menambahkan database course dan blog

--- backend/convert-base64.js
+++ backend/convert-base64.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-const file = "./video-belajar-firebase-adminsdk.json";
+const file = "./serviceAccountKey.json";
 
 const base64 = fs.readFileSync(file).toString("base64");
 
